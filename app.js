f9740848a83e4cdf704077de831173cda76d4251
TemplePsicologia <psickellyhuamani@gmail.com>
2026-09-25 10:28:19 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+function openJourney(journey) {
+
+    alert(
+        "Has elegido el recorrido: " +
+        journey +
+        "\n\nEsta sección la construiremos en el siguiente paso."
+    );
+
+}
